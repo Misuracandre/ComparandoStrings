df350a7985f6ecbba8ddb0324ee931a1c5f477b8
André misuraca <m1sur4k@gmail.com>
2023-03-07 21:38:24 -0300

    Adicionar arquivos de projeto.

---
+++ ComparandoStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {    // Declaraçao de váriaveis
+        string nome1, nome2;
+
+        nome1 = Console.ReadLine();
+        nome2 = Console.ReadLine();
+
+        // nome1 é chamado no metodo equals, usando nome2 como argumento
+        bool saoIguais = nome1.Equals(nome2, StringComparison.OrdinalIgnoreCase);
+
+        if (saoIguais)
+        {
+            Console.WriteLine("As strings são iguais.");
+        }
+        else
+        {
+            Console.WriteLine("As strings são diferentes.");
+        }
+    }
+}
